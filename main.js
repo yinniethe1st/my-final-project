8e7bb761ffe89afd813a4c3a7af02bd743739654
yinniethe1st <yinniethe1st@yahoo.com>
2020-08-14 22:05:43 -0600

    final change javascript made

--- main.js
+++ main.js
@@ -2,104 +2,110 @@ const baseUrl = 'http://jservice.io/';
 
 // get html elements and put them in constant variables
 
-const   flashcardUrl = document.querySelector('#flashcards'),
-        cluesUl = document.querySelector('#clues'),
-        answerDiv = document.querySelector('#answerDiv'),
-        answerP = document.querySelector('#answer'),
-        answerul = document.querySelector('#answerul'),
-        btnDisplayAnswer = document.getElementById('btnDisplayAnswer');
-
+const answerDiv = document.querySelector('#answerDiv');
+const answerP = document.querySelector('#answer');
+const answerul = document.querySelector('#answerul');
 const btnChooseCategory = document.getElementById('btnChooseCategory');
+const btnDisplayAnswer = document.getElementById('btnDisplayAnswer');
+const cluesUl = document.querySelector('#clues');
+const flashcardUrl = document.querySelector('#flashcards');
+const guessButton = document.getElementById('guess-button');
+const userAnswerInput = document.getElementById('user-answer');
+const checkAnswer = document.querySelector('#check-answer');
 
-btnChooseCategory.addEventListener('click', (event) => { 
+function onChooseCategory(event) {
+    fetch(baseUrl + 'api/categories?count=100')
+        .then(res => res.json())
+        .then(flashcards => {
+            console.log(flashcards);
 
-fetch(baseUrl + 'api/categories?count=100')
-    .then(resp => resp.json())
-    .then(flashcards => {
-        console.log(flashcards)
-    // shuffle array
-        const shuffled = flashcards.sort(() => 0.5 - Math.random());
-    // Get sub-array of first 10 elements after shuffled
-        let selectedCats = shuffled.slice(0,5);
-        flashcardUrl.innerHTML= "";
+            // shuffle array
+            const shuffled = flashcards.sort(() => 0.5 - Math.random());
 
-        console.log(selectedCats)
+            // Get sub-array of first 10 elements after shuffled
+            let selectedCats = shuffled.slice(0, 5);
+            console.log(selectedCats);
 
-    selectedCats.map(flashcard => {
-        const li = document.createElement('li');
+            flashcardUrl.innerHTML = "";
 
-        li.innerHTML = flashcard.title;
-        
-        li.setAttribute('flashcardid', flashcard.id);
-        
-        li.addEventListener('click', (event) => {
-            const currentFlashcardid = event.target.getAttribute('flashcardid');
-            console.log(event.target.getAttribute('flashcardid'));
+            selectedCats.map(flashcard => {
+                const liCategory = document.createElement('div');
+                liCategory.className = 'myclass';
 
-            // clear the clues ul and answerP
-            cluesUl.innerHTML = "";
-            answerP.innerHTML = "";
-            question.innerHTML = "";
+                liCategory.innerHTML = flashcard.title;
+                liCategory.setAttribute('flashcardid', flashcard.id);
 
-            answerDiv.setAttribute('style', 'display: none;');
+                liCategory.addEventListener('click', (event) => {
+                    const currentFlashcardid = event.target.getAttribute('flashcardid');
+                    console.log(event.target.getAttribute('flashcardid'));
 
-            getDataService(currentFlashcardid);
+                    // clear the clues ul and answerP
+                    cluesUl.innerHTML = "";
+                    answerP.innerHTML = "";
+                    question.innerHTML = "";
+
+                    answerDiv.setAttribute('style', 'display: none;');
+
+                    getDataService(currentFlashcardid);
+                })
+
+                flashcardUrl.appendChild(liCategory)
+            })
         })
+}
 
-        flashcardUrl.appendChild(li)
-
-    })
-})
-
-})
-
+btnChooseCategory.addEventListener('click', onChooseCategory);
 
 const getDataService = (currentFlashcardid) => {
-
     fetch(baseUrl + 'api/category?id=' + currentFlashcardid)
-                .then(resp => resp.json())
-                .then(data => { 
+        .then(res => res.json())
+        .then(data => {
+            const clues = data.clues;
+            console.log(clues)
 
-                    const clues = data.clues;
+            clues.map(clue => {
+                console.log(clue)
 
-                    console.log(clues)
+                const li = document.createElement('li');
+
+                //proper use of variables with scope
+                li.innerHTML = ` Difficulty: ${clue.value}`;
+
+                // add event listener to the clues
+                li.addEventListener('click', (event) => {
+                    // show the question Div
+                    questionDiv.setAttribute('style', 'display: block;');
+                    question.innerHTML = clue.question;
                     
-                    clues.map(clue => {
+                    guessButton.addEventListener('click', () => {
+                        const userAnswer = userAnswerInput.value;
+                        
+                        // I do this to make sure the user actually entered
+                        // something
+                        if (userAnswer) {
+                            const userAnswerLowercase = userAnswer.toLowerCase();
+                            const clueLowercase = clue.answer.toLowerCase();
+                            
+                            if (userAnswerLowercase === clueLowercase) {
 
-                        console.log(clue)
+                                checkAnswer.innerHTML = "True";
 
-                        const li = document.createElement('li');
-
-
-
-                        //proper use of variables with scope
-                        li.innerHTML= ` Difficulty: ${clue.value}`;
-
-                        // add event listener to the clues
-
-                        li.addEventListener('click', (event) => {
-
-                            // show the question Div
-                            questionDiv.setAttribute('style', 'display: block;');
-
-                            question.innerHTML = clue.question;
-
-                            btnDisplayAnswer.addEventListener('click', (event) => {
-
-                                answerDiv.setAttribute('style', 'display: block;');
-    
-                                answerP.innerHTML = clue.answer;
-    
-    
-                                    })
-                        })
-
-        
-
-                        cluesUl.appendChild(li);
+                            } else {
+                                checkAnswer.innerHTML = "False";
+                            }
+                        } else {
+                            checkAnswer.innerHTML = "Please enter a valid input";
+                        }
+                    });
+                    
+                    // I am commenting this part out because it is no longer needed
+                    btnDisplayAnswer.addEventListener('click', (event) => {
+                        answerDiv.setAttribute('style', 'display: block;');
+                        answerP.innerHTML = clue.answer;
                     })
                 })
 
+                cluesUl.appendChild(li);
+            })
+        })
 }
-
-
